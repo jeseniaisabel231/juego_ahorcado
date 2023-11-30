83ec5b5446225fdd0cc09aa2578fcc83f39acab0
jeseniaisabel231 <jesenia.pazto@epn.edu.ec>
2023-11-30 16:04:06 -0500

    avance

--- Main.java
+++ Main.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Random rand = new Random();
+        //Numero aleatorio de 1 a 10
+        int randomIndex = rand.nextInt(10);
+        String[] words = {"objetos", "datos", "analisis", "gestion", "interfaz", "diseño", "programacion", "bases", "proyectos"};
+
+        String word = words[randomIndex];
+        int letters = word.length();
+
+        char[] guess = new char[letters];
+
+    }
 
 
-}
+
+    }
